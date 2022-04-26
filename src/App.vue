d0347b10843f82f1deec0998531878ed71f54d24
Bers4658 <gorbunov4658@yandex.ru>
2022-04-26 15:28:38 +0300

    Регистрация

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+
+    <v-list>
+      <v-list-item v-for="link in links" :key="link.title" :to="link.url">
+        <v-list-item-icon>
+          <v-icon>{{ link.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="link.title"></v-list-item-title>
+        </v-list-item-content>
+
+        <v-list-item-avatar>
+          <v-img></v-img>
+        </v-list-item-avatar>
+      </v-list-item>
+    </v-list>
+
+    </v-navigation-drawer>
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+      <v-toolbar-title>Ad aplication</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn v-for="link in links" :key="link.title" :to="link.url" color="primary">
+          <v-icon left>
+            {{link.icon}}
+          </v-icon>
+          {{link.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  export default {
+    data: () => ({ 
+      drawer: false,
+      links: [
+        {title: 'Login', icon: 'mdi-lock', url: '/login'},
+        {title: 'Registrator', icon: 'mdi-face-man-outline', url: '/registrator'},
+        {title: 'Orders', icon: 'mdi-wallet-membership', url: '/orders'},
+        {title: 'New ad', icon: 'mdi-plus-box-multiple', url: '/new'},
+        {title: 'My ads', icon: 'mdi-playlist-plus', url: '/list'},
+      ]
+    }),
   }
-}
-</style>
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,53 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeVue from '@/components/HomeVue'
+import AdVue from '@/components/Ads/AdVue'
+import AddList from '@/components/Ads/AddList'
+import NewAd from '@/components/Ads/NewAd'
+import LoginVue from '@/components/Auth/LoginVue'
+import RegistrationVue from '@/components/Auth/RegistrationVue'
+import OrdersVue from '@/components/User/OrdersVue'
+
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+export default new VueRouter ({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeVue
+    },
+    {
+      path: '/ad',
+      name: 'ad',
+      component: AdVue
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: AddList
+    },
+    {
+      path: '/new',
+      name: 'newAd',
+      component: NewAd
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginVue
+    },
+    {
+      path: '/registrator',
+      name: 'reg',
+      component: RegistrationVue
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: OrdersVue
+    },
+  ],
+  mode: 'history'
 })
-
-export default router
